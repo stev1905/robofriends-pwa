9bb21f8bb7f90b1f441226a235b71a105c7ec5c1
Christian Stevens <stev1905@gmail.com>
2020-05-15 23:12:15 -0400

    Updates

---
+++ precache-manifest.57204652d5556ec7baa5c81d2336569e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdf91004002ffc3d93f0bd745d4f5f64",
+    "url": "/stev1905/robofriends-pwa/index.html"
+  },
+  {
+    "revision": "2b43e9665a2272a1d379",
+    "url": "/stev1905/robofriends-pwa/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "4bdb20f2fe25d3c4754b",
+    "url": "/stev1905/robofriends-pwa/static/css/main.d0b74e20.chunk.css"
+  },
+  {
+    "revision": "2b43e9665a2272a1d379",
+    "url": "/stev1905/robofriends-pwa/static/js/2.a024ff84.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/stev1905/robofriends-pwa/static/js/2.a024ff84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bdb20f2fe25d3c4754b",
+    "url": "/stev1905/robofriends-pwa/static/js/main.5387781e.chunk.js"
+  },
+  {
+    "revision": "6eba9fbff55e402320e8",
+    "url": "/stev1905/robofriends-pwa/static/js/runtime-main.f34d1f7e.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/stev1905/robofriends-pwa/static/media/SEGA.12a3db8b.woff"
+  }
+]);
